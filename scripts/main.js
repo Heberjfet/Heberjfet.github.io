46c7ef8c303f2330b27d5fd042b65bcee0656b35
Heberjfet <114103375+Heberjfet@users.noreply.github.com>
2024-05-03 23:42:55 -0600

    feat: Add event listeners for aside button and subHead
    This commit adds event listeners for the aside button and subHead elements in the main.js file. The event listener for the aside button toggles the 'activo' class on the aside element, allowing it to expand and collapse. The event listener for the subHead element removes the 'activo' class from the aside element, ensuring it collapses when clicked. These changes improve the functionality and user experience of the website.
    
    Co-authored-by: breadstack

--- scripts/main.js
+++ scripts/main.js
@@ -6,29 +6,21 @@ import { Tab, initMDB } from "mdb-ui-kit";
 initMDB({ Tab });
 import { Ripple, initMDB } from "mdb-ui-kit";
 // libreria de MDA
+initMDB({ Ripple });
 
 //codigo del aside
-initMDB({ Ripple });
-const boton = document.getElementById('boton');
-const aside = document.getElementById('lateral');
-const subHead = document.getElementById('sub-head');
-var isActive = false;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const boton = document.getElementById('boton');
+    const aside = document.getElementById('lateral');
+    const subHead = document.getElementById('sub-head');
 
-//evento del boton
-boton.addEventListener('click', function () {
-    if (!isActive) {
-        aside.classList.add('activo');
-        isActive = !isActive;
-    }
-    else {
-        aside.classList.remove('activo');
-        isActive = !isActive;
-    }
-});
-subHead.addEventListener('click', function () {
-    if (isActive) {
-        aside.classList.remove('activo');
-        isActive = !isActive;
-    }
-});
+    //evento del boton
+    boton.addEventListener('click', function () {
+        aside.classList.toggle('activo');
+    });
 
+    //evento del subHead
+    subHead.addEventListener('click', function () {
+        aside.classList.remove('activo');
+    });
+});
